ccc2e51c6db78dff19936b49727aff074dc3cef1
Bazaah <github@luxolus.com>
2020-02-27 11:40:29 +0000

    loopframe => async

--- loopframe/src/models.rs
+++ loopframe/src/models.rs
@@ -1,35 +1,20 @@
 use {
-    crate::ARGS,
-    serde_cbor::Deserializer,
-    serde_interface::Record,
+    crate::{prelude::*, ARGS},
+    futures::prelude::*,
+    serde_interface::{Record, RecordInterface},
     serde_json::{to_writer, to_writer_pretty},
-    std::{error, io, net::TcpListener},
+    std::{io, net::SocketAddr, path::Path},
+    tokio::{net::TcpListener, prelude::AsyncRead},
+    tracing_subscriber::{EnvFilter, FmtSubscriber},
 };
 
-pub fn process_single_stream() -> Result<(), Box<dyn error::Error>> {
+pub async fn process_single_stream() -> Result<(), io::Error> {
     match (ARGS.con_socket(), ARGS.con_tcp()) {
         (Some(socket), _) => {
             if cfg!(target_family = "unix") {
-                use std::os::unix::net::UnixListener;
-                let listener = UnixListener::bind(socket).map_err(|e| Box::new(e))?;
-                listener
-                    .accept()
-                    .map_err(|e| Box::new(e) as Box<dyn error::Error>)
-                    .and_then(|(stream, addr)| {
-                        println!("Got a connection from: {:?}", addr);
-                        let mut iter = stream_transcode(stream);
-                        let p = ARGS.pretty_print();
-                        while let Some(record) = iter.next() {
-                            match record? {
-                                Record::StreamStart => (),
-                                Record::StreamEnd => break,
-                                record @ _ => print_json(p, io::stdout(), record)?,
-                            }
-                        }
-                        Ok(())
-                    })?;
-
-                Ok(())
+                use_unixsocket(socket)
+                    .instrument(always_span!("server.unixsocket", socket = %socket.display()))
+                    .await
             } else {
                 // Should not be possible to hit this path as con_socket() should always return None on
                 // non-unix systems
@@ -37,40 +22,67 @@ pub fn process_single_stream() -> Result<(), Box<dyn error::Error>> {
             }
         }
         (_, Some(addr)) => {
-            let listener = TcpListener::bind(addr).map_err(|e| Box::new(e))?;
-            listener
-                .accept()
-                .map_err(|e| Box::new(e) as Box<dyn error::Error>)
-                .and_then(|(stream, addr)| {
-                    println!("Got a connection from: {:?}", addr);
-                    let mut iter = stream_transcode(stream);
-                    let p = ARGS.pretty_print();
-                    while let Some(record) = iter.next() {
-                        match record? {
-                            Record::StreamStart => (),
-                            Record::StreamEnd => break,
-                            record @ _ => print_json(p, io::stdout(), record)?,
-                        }
-                    }
-                    Ok(())
-                })?;
-
-            Ok(())
+            use_tcp(addr)
+                .instrument(always_span!("server.tcp", socket = %addr))
+                .await
         }
         _ => unreachable!(),
     }
 }
 
-fn stream_transcode<R>(reader: R) -> impl Iterator<Item = Result<Record, Box<dyn error::Error>>>
-where
-    R: io::Read,
-{
-    Deserializer::from_reader(reader)
-        .into_iter::<Record>()
-        .map(|res| res.map_err(|e| e.into()))
+#[cfg(unix)]
+async fn use_unixsocket(socket: &Path) -> Result<(), io::Error> {
+    use tokio::net::UnixListener;
+    debug!("Attempting to bind {}...", socket.display());
+    UnixListener::bind(socket)
+        .map(|l| {
+            info!("Bind successful, server is waiting on connections");
+            l
+        })
+        .map_err(|e| {
+            error!("Binding {} failed... bailing", socket.display());
+            e
+        })?
+        .accept()
+        .inspect_ok(|(_, client)| {
+            client
+                .as_pathname()
+                .map(|p| info!("Accepted connection from: {}", p.display()))
+                .unwrap_or_else(|| info!("Accepted connection from: unnamed"))
+        })
+        .and_then(|(socket, _)| handle_connection(socket).map(|_| Ok(())))
+        .await
 }
 
-fn print_json<W>(pretty: bool, writer: W, rcd: Record) -> Result<(), Box<dyn error::Error>>
+async fn use_tcp(addr: SocketAddr) -> Result<(), io::Error> {
+    debug!("Attempting to bind {}...", addr);
+    TcpListener::bind(addr)
+        .inspect(|status| match status {
+            Ok(_) => info!("Bind successful, server is waiting on connections"),
+            Err(_) => error!("Binding {} failed... bailing", addr),
+        })
+        .await?
+        .accept()
+        .inspect_ok(|(_, client)| info!("Accepted connection from: {}", client))
+        .and_then(|(socket, _)| handle_connection(socket).map(|_| Ok(())))
+        .await
+}
+
+async fn handle_connection<T>(read: T)
+where
+    T: AsyncRead,
+{
+    let pretty = ARGS.pretty_print();
+    RecordInterface::from_read(read)
+        .for_each(|item| async {
+            item.and_then(|record| print_json(pretty, io::stdout(), record))
+                .unwrap_or_else(|e| warn!("Item serialization failed: {}", e))
+        })
+        .instrument(always_span!("printer.json", pretty))
+        .await
+}
+
+fn print_json<W>(pretty: bool, writer: W, rcd: Record) -> Result<(), io::Error>
 where
     W: io::Write,
 {
@@ -79,4 +91,18 @@ where
         false => to_writer(writer, &rcd)?,
     }
     Ok(())
+}
+
+/// Initialize the global logger. This function must be called before ARGS is initialized,
+/// otherwise logs generated during CLI parsing will be silently ignored
+pub fn init_logging() {
+    let root_subscriber = FmtSubscriber::builder()
+        .with_writer(std::io::stderr)
+        .with_env_filter(EnvFilter::try_from_default_env().unwrap_or_else(|_| {
+            EnvFilter::default().add_directive(tracing_subscriber::filter::LevelFilter::INFO.into())
+        }))
+        .with_filter_reloading()
+        .finish();
+    tracing::subscriber::set_global_default(root_subscriber).expect("Failed to init logging");
+    info!("<== Logs Start ==>")
 }
